42d6ce2f7dc17540404c6ef98f4e084fd17f3660
Humberto Menezes <hmcf@cesar.org.br>
2014-08-01 11:19:53 -0300

    added feed mediator and database js

---
+++ js/feed_database.js
@@ -0,0 +1,17 @@
+
+/*
+ * Class for persisting and retrieving feed information stored in the database. 
+ */
+function FeedDatabase() {
+	
+} 
+FeedDatabase.prototype = {};
+
+/*
+ * Creates the database.
+ */
+FeedDatabase.prototype.initialize = function() {
+	
+	// initializing the variable that will hold the database file name and location
+	
+}
